bfe0fbe781f6850fe3b15d4f19d855e721e75696
marrikarunyareddy <143412334+MarriKarunyaReddy@users.noreply.github.com>
2025-04-23 06:01:46 +0000

    updated-cognito-config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_TOsz3R4z1', 
+        userPoolClientId: '6v48jjr3b0hoiitdh7c6j4h6u2', 
+        region: 'us-east-1' 
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
